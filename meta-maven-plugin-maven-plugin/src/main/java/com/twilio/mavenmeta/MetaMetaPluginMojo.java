bc2e80112c0a69317b633b5bd0bb9a515ef6c206
Ryan Michela <rmichela@twilio.com>
2025-03-27 10:33:26 -0400

    WIP

--- meta-maven-plugin-maven-plugin/src/main/java/com/twilio/mavenmeta/MetaMetaPluginMojo.java
+++ meta-maven-plugin-maven-plugin/src/main/java/com/twilio/mavenmeta/MetaMetaPluginMojo.java
@@ -1,18 +1,22 @@
 package com.twilio.mavenmeta;
 
+import org.apache.maven.execution.MavenSession;
 import org.apache.maven.model.PluginContainer;
+import org.apache.maven.model.PluginExecution;
 import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecution;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.*;
+import org.apache.maven.plugin.descriptor.MojoDescriptor;
+import org.apache.maven.plugin.descriptor.PluginDescriptor;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.plugins.annotations.Component;
 import org.codehaus.plexus.component.annotations.Requirement;
 import org.codehaus.plexus.util.xml.*;
 import org.codehaus.plexus.util.xml.pull.MXParser;
 import org.codehaus.plexus.util.xml.pull.XmlPullParser;
+import org.twdata.maven.mojoexecutor.MojoExecutor;
 
 import javax.inject.Inject;
 import java.io.ByteArrayInputStream;
@@ -21,27 +25,48 @@ import java.lang.reflect.Method;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 
-@Mojo(name = "generate-meta-plugin", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
+@Mojo(name = "generate-meta-plugin", defaultPhase = LifecyclePhase.INITIALIZE, threadSafe = true)
 public class MetaMetaPluginMojo extends AbstractMojo {
 
     @Inject
     private MojoExecution mojoExecution;
 
+    @Inject
+    private BuildPluginManager pluginManager;
+
+    @Parameter(defaultValue = "${project}", readonly = true, required = true)
+    private MavenProject project;
+
+    /**
+     * The current Maven session.
+     */
+    @Parameter(defaultValue = "${session}", readonly = true, required = true)
+    private MavenSession mavenSession;
+
+
     @Parameter(name = "plugins", required = true)
     private List<Plugin> plugins;
 
     @Override
-    public void execute() throws MojoExecutionException, MojoFailureException {
+    public void execute() throws MojoExecutionException {
         try {
             var plugin = mojoExecution.getPlugin();
             Xpp3Dom configurationXml = (Xpp3Dom) plugin.getConfiguration();
             if (configurationXml != null) {
                 getLog().info("Received XML configuration:");
-//                printXml(configurationXml);
-                plugins.forEach(p -> {
+                for (Plugin p : plugins) {
                     getLog().info(p.getGroupId() + ":" + p.getArtifactId() + ":" + p.getVersion());
-                    getLog().info(p.getConfiguration().toString());
-                });
+
+                    for (PluginExecution ex : p.getExecutions()) {
+                        for (String goal : ex.getGoals()) {
+                            MojoExecutor.executeMojo(
+                                    p,
+                                    goal,
+                                    (Xpp3Dom) p.getConfiguration(),
+                                    MojoExecutor.executionEnvironment(project, mavenSession, pluginManager));
+                        }
+                    }
+                }
             } else {
                 getLog().warn("No XML configuration provided.");
             }
@@ -50,24 +75,24 @@ public class MetaMetaPluginMojo extends AbstractMojo {
         }
     }
 
-    private void printXml(Xpp3Dom dom) {
-        StringWriter writer = new StringWriter();
-        XMLWriter xmlWriter = new PrettyPrintXMLWriter(writer, "  ");
-        Xpp3DomWriter.write(xmlWriter, dom, false);
-        getLog().info("\n" + writer);
-    }
-
-    private PluginContainer getPluginContainer(Xpp3Dom pluginContainerXml) throws Exception {
-        Method privateMethod = MavenXpp3Reader.class.getDeclaredMethod("parsePluginContainer", XmlPullParser.class, boolean.class);
-        privateMethod.setAccessible(true);
-
-        StringWriter writer = new StringWriter();
-        XMLWriter xmlWriter = new CompactXMLWriter(writer);
-        Xpp3DomWriter.write(xmlWriter, pluginContainerXml, false);
-
-        XmlPullParser parser = new MXParser();
-        parser.setInput(new XmlStreamReader(new ByteArrayInputStream(writer.toString().getBytes(StandardCharsets.UTF_8))));
-        parser.nextTag(); // Discard the opening configuration tag
-        return (PluginContainer) privateMethod.invoke(new MavenXpp3Reader(),parser, true);
-    }
+//    private void printXml(Xpp3Dom dom) {
+//        StringWriter writer = new StringWriter();
+//        XMLWriter xmlWriter = new PrettyPrintXMLWriter(writer, "  ");
+//        Xpp3DomWriter.write(xmlWriter, dom, false);
+//        getLog().info("\n" + writer);
+//    }
+//
+//    private PluginContainer getPluginContainer(Xpp3Dom pluginContainerXml) throws Exception {
+//        Method privateMethod = MavenXpp3Reader.class.getDeclaredMethod("parsePluginContainer", XmlPullParser.class, boolean.class);
+//        privateMethod.setAccessible(true);
+//
+//        StringWriter writer = new StringWriter();
+//        XMLWriter xmlWriter = new CompactXMLWriter(writer);
+//        Xpp3DomWriter.write(xmlWriter, pluginContainerXml, false);
+//
+//        XmlPullParser parser = new MXParser();
+//        parser.setInput(new XmlStreamReader(new ByteArrayInputStream(writer.toString().getBytes(StandardCharsets.UTF_8))));
+//        parser.nextTag(); // Discard the opening configuration tag
+//        return (PluginContainer) privateMethod.invoke(new MavenXpp3Reader(),parser, true);
+//    }
 }
--- meta-maven-plugin-maven-plugin/src/main/java/com/twilio/mavenmeta/Plugin.java
+++ meta-maven-plugin-maven-plugin/src/main/java/com/twilio/mavenmeta/Plugin.java
@@ -1,19 +1,12 @@
 package com.twilio.mavenmeta;
 
-import org.codehaus.plexus.configuration.xml.XmlPlexusConfiguration;
-import org.codehaus.plexus.util.xml.Xpp3Dom;
-import org.codehaus.plexus.util.xml.Xpp3DomBuilder;
-import org.codehaus.plexus.util.xml.pull.MXParser;
-import org.codehaus.plexus.util.xml.pull.XmlPullParser;
-
-import java.io.StringReader;
+import org.codehaus.plexus.configuration.PlexusConfiguration;
+import org.twdata.maven.mojoexecutor.PlexusConfigurationUtils;
 
 public class Plugin extends org.apache.maven.model.Plugin {
     // Short circuit SISU/Plexus dependency injection for the Plugin class's configuration field. Setter injection
     // takes priority over field injection when resolving injection.
-    public void setConfiguration(XmlPlexusConfiguration xml) throws Exception {
-        StringReader reader = new StringReader(xml.toString());
-        Xpp3Dom dom = Xpp3DomBuilder.build(reader);
-        super.setConfiguration(dom);
+    public void setConfiguration(PlexusConfiguration xml) throws Exception {
+        super.setConfiguration(PlexusConfigurationUtils.toXpp3Dom(xml));
     }
 }
