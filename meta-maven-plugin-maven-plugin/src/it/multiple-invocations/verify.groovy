d325cc85deded339b2d5f1cfc7b17fa0a67ef1a7
Ryan Michela <rmichela@twilio.com>
2025-05-29 15:21:46 -0400

    Enhance the multiple-invocations test case

--- meta-maven-plugin-maven-plugin/src/it/multiple-invocations/verify.groovy
+++ meta-maven-plugin-maven-plugin/src/it/multiple-invocations/verify.groovy
@@ -1,8 +1,13 @@
 // Assert the plugin was generated correctly
-assert new File(basedir, "/target/generated-sources/meta-maven-plugin/io/github/rmichela/test_case_multiple_invocations_maven_plugin/InitializeMojo.java").exists()
-assert new File(basedir, "/target/generated-sources/meta-maven-plugin/io/github/rmichela/test_case_multiple_invocations_maven_plugin/HelloInitializeMojo.java").exists()
-assert new File(basedir, "/target/generated-sources/meta-maven-plugin/io/github/rmichela/test_case_multiple_invocations_maven_plugin/HelloAbstractMetaPluginMojo.java")
+assert new File(basedir, "/plugin/target/generated-sources/meta-maven-plugin/io/github/rmichela/test_case_multiple_invocations_maven_plugin/InitializeMojo.java").exists()
+assert new File(basedir, "/plugin/target/generated-sources/meta-maven-plugin/io/github/rmichela/test_case_multiple_invocations_maven_plugin/HelloInitializeMojo.java").exists()
+assert new File(basedir, "/plugin/target/generated-sources/meta-maven-plugin/io/github/rmichela/test_case_multiple_invocations_maven_plugin/HelloAbstractMetaPluginMojo.java")
         .text.contains("Hello World")
-assert new File(basedir, "/target/generated-sources/meta-maven-plugin/io/github/rmichela/test_case_multiple_invocations_maven_plugin/GoodbyeInitializeMojo.java").exists()
-assert new File(basedir, "/target/generated-sources/meta-maven-plugin/io/github/rmichela/test_case_multiple_invocations_maven_plugin/GoodbyeAbstractMetaPluginMojo.java")
+assert new File(basedir, "/plugin/target/generated-sources/meta-maven-plugin/io/github/rmichela/test_case_multiple_invocations_maven_plugin/GoodbyeInitializeMojo.java").exists()
+assert new File(basedir, "/plugin/target/generated-sources/meta-maven-plugin/io/github/rmichela/test_case_multiple_invocations_maven_plugin/GoodbyeAbstractMetaPluginMojo.java")
         .text.contains("Goodbye World")
+
+// Assert the plugin executed correctly
+assert new File(basedir, "/build.log").text.contains("Hello DEFAULT World")
+assert new File(basedir, "/build.log").text.contains("Hello World")
+assert new File(basedir, "/build.log").text.contains("Goodbye World")
